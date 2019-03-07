08be9821080fe24303341ce0c764e62629430981
Gethyn Jones <gethyn1@gmail.com>
2019-03-07 12:42:29 +0000

    Add ant design react library

--- src/app/views/layouts/app.jsx
+++ src/app/views/layouts/app.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import { Layout, Typography } from 'antd'
+
+import 'antd/dist/antd.css'
+
+const { Header, Content } = Layout
+const { Title } = Typography
 
 const AppLayout = ({ children }) => (
-  <React.Fragment>
-    <h1>Craft &amp; Local</h1>
-    {children}
-  </React.Fragment>
+  <Layout>
+    <Header>
+      <Title style={{ color: '#fff' }}>Craft &amp; Local</Title>
+    </Header>
+    <Content style={{ padding: '0 50px' }}>
+      {children}
+    </Content>
+  </Layout>
 )
 
 export {
